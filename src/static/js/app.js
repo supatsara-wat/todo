bf28c52b2401996865910b2d12fb51a5ca0bb5d0
Supatsara <supatsara-wa@Supatsaras-MacBook-Pro.local>
2020-12-27 18:35:14 +0900

    edited

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -3,9 +3,6 @@ function App() {
     return (
         <Container>
             <Row>
-            <Col>Todo App</Col>
-            </Row>
-            <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
                 </Col>
@@ -56,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">No items yet!??? Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
