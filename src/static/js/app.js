b14d651e5f4454e1b596b0a0356bd7ddf9372475
Supatsara <supatsara-wa@Supatsaras-MacBook-Pro.local>
2020-12-27 18:07:48 +0900

    edited a file

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,6 +2,7 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
+            <h1>Todo App</h1>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
